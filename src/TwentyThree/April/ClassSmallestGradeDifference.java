5cf8230e53ba0b53ced42de5354d7957c1e8e43d
tjdxo11933 <tjdxo11933@naver.com>
2023-04-19 16:13:23 +0900

    도중에 베트남으로..

--- src/TwentyThree/April/ClassSmallestGradeDifference.java
+++ src/TwentyThree/April/ClassSmallestGradeDifference.java
@@ -1,5 +1,8 @@
 package TwentyThree.April;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClassSmallestGradeDifference {
 
 //     오름차순으로 정렬된 성적들 int 배열과, int n 줄때
@@ -21,11 +24,53 @@ public class ClassSmallestGradeDifference {
 //
 //        1 / 2, 3
 //        1, 2 / 3
+
+        solution(n, nClass);
     }
 
-    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-        long answer = -1;
-        return answer;
+    class Group{
+        List<Integer> scores;
+        public Group(List<Integer> scores) {
+            this.scores = scores;
+        }
     }
 
+    static List<Group> groups = new ArrayList<>();
+    static int minResult = 0;
+
+    public static int solution(int nthClass, int[] scores) {
+        dfs(1, nthClass, scores);
+        return minResult;
+    }
+
+    /*
+    시간복잡도 n^2!
+4
+1,2,3,4,5,6,7,8
+                        root
+                   1                        1,2          1,2,3     1,2,3,4         1,2,3,4,5        .... (m.length - 4 + 1)
+        2    2,3   2,3,4  2,3,4,5  2,3,4,5,6  - 4      3             1                              ..... 5*4*3*2*1 4!
+      3456  4,5,6  5,6    6                                                                                     4!
+
+    */
+        // 다음 class 반
+    public static void dfs(int depth, int nthClass, int[] scores){
+        // 종료 및 계산 조건 - 반이 다 만들어 졌을떄 즉, depth 가 n일때.
+        if (depth == nthClass) {
+            int scoreSum = 0;
+
+
+            if(scoreSum > minResult) {
+                minResult = scoreSum;
+            }
+            return;
+        }
+
+        for (int i = 0; i < nthClass; i++ ){
+//            groups.add(new groups());
+//            하나 선택하고 보내고 하나에 하나를 더해서 선택하고 보내고
+            dfs(depth+1, nthClass, scores);
+
+        }
+    }
 }
