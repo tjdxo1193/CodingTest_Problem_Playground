59dc3e5556439fe577fd8750284edda8196da439
tjdxo11933 <tjdxo11933@naver.com>
2023-06-20 11:41:21 +0900

    콜라문제

--- src/TwentyThree/June/codingTest/CokeProblem.java
+++ src/TwentyThree/June/codingTest/CokeProblem.java
@@ -1,2 +1,13 @@
-package TwentyThree.June.codingTest;public class CokeProblem {
+package TwentyThree.June.codingTest;
+
+public class CokeProblem {
+    public static void main(String[] args) {
+
+
+    }
+
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        return answer;
+    }
 }
