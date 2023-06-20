9b31089040dd719ce0236f54ba956f5acd399cd3
tjdxo11933 <tjdxo11933@naver.com>
2023-06-20 11:41:29 +0900

    콜라문제

--- src/TwentyThree/June/codingTest/CokeProblem.java
+++ src/TwentyThree/June/codingTest/CokeProblem.java
@@ -8,6 +8,11 @@ public class CokeProblem {
 
     public int solution(int a, int b, int n) {
         int answer = 0;
+        while (n / a > 0) {
+
+            answer += (n / a) * b;
+            n = (n / a) * b + n % a;
+        }
         return answer;
     }
 }
