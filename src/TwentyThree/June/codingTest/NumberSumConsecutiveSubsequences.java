813ceea77007f5ff1f7ac50c860b288f4341b7e1
tjdxo11933 <tjdxo11933@naver.com>
2023-06-20 18:14:12 +0900

    연속된시퀀스

--- src/TwentyThree/June/codingTest/NumberSumConsecutiveSubsequences.java
+++ src/TwentyThree/June/codingTest/NumberSumConsecutiveSubsequences.java
@@ -1,2 +1,31 @@
-package TwentyThree.June.codingTest;public class NumberSumConsecutiveSubsequences {
+package TwentyThree.June.codingTest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NumberSumConsecutiveSubsequences {
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int[] elements) {
+        Set<Integer> answer = new HashSet<>();
+
+        for (int i = 1; i <= elements.length; i++) {
+            for (int j = 0; j < elements.length; j++) {
+                int sum = 0;
+                for (int k = 0; k < i; k++) {
+                    if (j + k > elements.length - 1) {
+                        sum += elements[j + k - elements.length];
+                    } else {
+                        sum += elements[j + k];
+                    }
+                }
+                answer.add(sum);
+
+            }
+        }
+
+        return answer.size();
+    }
 }
