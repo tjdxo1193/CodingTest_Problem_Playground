308d40e4f51848cbad0e99a849f48cfc05cf640c
tjdxo11933 <tjdxo11933@naver.com>
2023-06-20 16:25:18 +0900

    메모리점수

--- src/TwentyThree/June/codingTest/MemoryScore.java
+++ src/TwentyThree/June/codingTest/MemoryScore.java
@@ -1,2 +1,21 @@
-package TwentyThree.June.codingTest;public class MemoryScore {
+package TwentyThree.June.codingTest;
+
+import java.util.Arrays;
+
+public class MemoryScore {
+    public static void main(String[] args) {
+    }
+
+
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        for (int i = 0; i < photo.length; i++) {
+            for (int j = 0; j < photo[i].length; j++) {
+                if (Arrays.asList(name).indexOf(photo[i][j]) != -1) {
+                    answer[i] += yearning[Arrays.asList(name).indexOf(photo[i][j])];
+                }
+            }
+        }
+        return answer;
+    }
 }
