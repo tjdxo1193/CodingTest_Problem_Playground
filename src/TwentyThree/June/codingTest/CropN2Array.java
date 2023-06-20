c692031adeae5b9505f977291a77a69e36a932a8
tjdxo11933 <tjdxo11933@naver.com>
2023-06-20 18:14:43 +0900

    N^2배열

--- src/TwentyThree/June/codingTest/CropN2Array.java
+++ src/TwentyThree/June/codingTest/CropN2Array.java
@@ -1,2 +1,19 @@
-package TwentyThree.June.codingTest;public class CropN2Array {
+package TwentyThree.June.codingTest;
+
+public class CropN2Array {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] solution(int n, long left, long right) {
+        int[] answer = new int[(int) right - (int) left + 1];
+
+        for (long i = left; i <= right; i++) {
+            long row = i / n;
+            long col = i % n;
+            answer[(int)(i-left)] = Math.max((int)row, (int)col) + 1;
+        }
+
+        return answer;
+    }
 }
