6b3fdfa98226faf732669c5efd6445acdd7700d4
tjdxo11933 <tjdxo11933@naver.com>
2023-06-20 18:12:50 +0900

    명예의 전당

--- src/TwentyThree/June/codingTest/HallOfFame.java
+++ src/TwentyThree/June/codingTest/HallOfFame.java
@@ -1,2 +1,22 @@
-package TwentyThree.June.codingTest;public class HallOfFame {
+package TwentyThree.June.codingTest;
+
+import java.util.PriorityQueue;
+
+public class HallOfFame {
+
+    public static void main(String[] args) {
+
+    }
+    public int[] solution(int[] score, int k) {
+        int[] result = new int[score.length];
+        PriorityQueue<Integer> rank = new PriorityQueue<>();
+
+        for (int i = 0; i < score.length; i++) {
+            rank.add(score[i]);
+            if (i >= k) rank.poll();
+            result[i] = rank.peek();
+        }
+        return result;
+    }
+
 }
