002072eb4c7597133775e98ffbf5a4e049bb9473
황성태 <tjdxo11933@naver.com>
2023-06-24 18:00:41 +0900

    모의고사

---
+++ src/TwentyThree/June/codingTest/MockExam.java
@@ -0,0 +1,66 @@
+package TwentyThree.June.codingTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class MockExam {
+
+    /*문제 설명
+수포자는 수학을 포기한 사람의 준말입니다.
+수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다.
+수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
+
+1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+
+1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때,
+가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+
+제한 조건
+시험은 최대 10,000 문제로 구성되어있습니다.
+문제의 정답은 1, 2, 3, 4, 5중 하나입니다.
+가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요.
+
+     */
+
+    public static void main(String[] args) {
+    }
+
+    public int[] solution(int[] answers) {
+        int[] supoja1 = new int[] {1, 2, 3, 4, 5}; // 5
+        int[] supoja2 = new int[] {2, 1, 2, 3, 2, 4, 2, 5}; //8
+        int[] supoja3 = new int[] {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; // 10
+        List<Integer> supojaPoint = new ArrayList<>();
+        List<Integer> maxScoreUser = new ArrayList<>();
+        supojaPoint.add(0);
+        supojaPoint.add(0);
+        supojaPoint.add(0);
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == supoja1[i % 5]) {
+                supojaPoint.set(0, supojaPoint.get(0) + 1);
+            }
+            if (answers[i] == supoja2[i % 8]) {
+                supojaPoint.set(1, supojaPoint.get(1) + 1);
+            }
+            if (answers[i] == supoja3[i % 10]) {
+                supojaPoint.set(2, supojaPoint.get(2) + 1);
+            }
+        }
+        int maxScore = Math.max(Math.max(supojaPoint.get(0), supojaPoint.get(1)), supojaPoint.get(2));
+        for (int i = 0; i < supojaPoint.size(); i++){
+            if (supojaPoint.get(i) == maxScore) {
+                maxScoreUser.add(i);
+            }
+        }
+
+        int answer[] = new int[maxScoreUser.size()];
+        int idx = 0;
+        for (Integer num : maxScoreUser) {
+            answer[idx++] = num+1;
+        }
+
+        return answer;
+    }
+}
