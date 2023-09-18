8f213e35148d8c9e850d04f36ea7dc0e7b4a7ede
tjdxo11933 <tjdxo11933@naver.com>
2023-09-18 13:10:18 +0900

    호텔 대실, 줄서기 방법 틀만

---
+++ src/TwentyThree/September/ReserveHotelRoom.java
@@ -0,0 +1,19 @@
+package TwentyThree.September;
+
+public class ReserveHotelRoom {
+    /*
+    문제 설명
+    호텔을 운영 중인 코니는 최소한의 객실만을 사용하여 예약 손님들을 받으려고 합니다.
+    한 번 사용한 객실은 퇴실 시간을 기준으로 10분간 청소를 하고 다음 손님들이 사용할 수 있습니다.
+    예약 시각이 문자열 형태로 담긴 2차원 배열 book_time이 매개변수로 주어질 때,
+    코니에게 필요한 최소 객실의 수를 return 하는 solution 함수를 완성해주세요.
+     */
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(String[][] book_time) {
+        int answer = 0;
+        return answer;
+    }
+}
